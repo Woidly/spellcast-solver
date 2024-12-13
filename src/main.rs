d1fc0f6b24da02660e97df7852eacef285b8e838
Woidly <woidly@proton.me>
2024-12-13 20:13:03 +0900

    feat: add basic program logic

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,36 @@ mod utils;
 
 fn main() {
     let args = args::parse();
-    println!("Args: {args:?}");
-    println!(
-        "Dictionary has {:?} entries",
-        dictionary::load_dictionary_file(&args.dictionary)
-            .unwrap()
-            .len()
-    )
+    // TODO: Maybe make dictionary loading a part of argument parsing (similar to board argument)?
+    let dictionary = match dictionary::load_dictionary_file(&args.dictionary) {
+        Ok(dictionary) => Box::leak(Box::new(dictionary)),
+        Err(e) => quit!("Failed to load dictionary: {e}"),
+    };
+    if args.swaps > 3 {
+        quit!("Swap count can't be higher than 3!")
+    }
+    let clock = std::time::Instant::now();
+    let (words, _) = spellcast::solver_wrapper(args.board, args.swaps, 1, dictionary);
+    let elapsed_ms = clock.elapsed().as_secs_f64() * 1000.;
+    println!("{elapsed_ms:.1}ms elapsed");
+    let mut existing_words = vec![];
+    let mut counter = 0;
+    let mut final_words = vec![];
+    for word in words {
+        if counter >= args.move_count {
+            break;
+        }
+        if existing_words.contains(&word.word) {
+            continue;
+        }
+        counter += 1;
+        existing_words.push((&word.word).clone());
+        final_words.push(word);
+    }
+    for (i, word) in final_words.into_iter().enumerate().rev() {
+        println!(
+            "{i}. {} (+{}pts, +{} gems, -{} swaps)",
+            word.word, word.score, word.gems_collected, word.swaps_used
+        );
+    }
 }
