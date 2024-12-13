9ae086d1196b788cb585ceae524254d628f26fb5
Woidly <woidly@proton.me>
2024-12-13 20:13:03 +0900

    feat: add basic program logic

--- src/utils.rs
+++ src/utils.rs
@@ -3,3 +3,14 @@
 /// Do not set it too low to keep words diverse (as it may include many different copies of same word).
 /// You can learn more in [crate::spellcast::SortedWordVec].
 pub const MAX_SOLUTIONS: usize = 256;
+
+pub const RED: &str = "\x1B[31m";
+pub const RESET: &str = "\x1B[0m";
+
+#[macro_export]
+macro_rules! quit {
+    ($($arg:tt)*) => {{
+        eprintln!("{}{}{}", crate::utils::RED, format!($($arg)*), crate::utils::RESET);
+        std::process::exit(1);
+    }};
+}
