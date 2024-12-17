2067124314a3a119230f67b691c0a0cf2f09b037
Woidly <woidly@proton.me>
2024-12-17 23:23:48 +0900

    feat(server): add basic server functionality

--- automatic/server/index.ts
+++ automatic/server/index.ts
@@ -1,14 +1,42 @@
 import parse from "./args";
 
-const args = await parse();
+const ARGS = await parse();
 
-console.log(`Starting server on port ${args.port}`);
-console.log(`Dictionary: ${args.dictionary}`);
-console.log(`Solver: ${args.solver}`);
+console.log(`Starting server on port ${ARGS.port}`);
+console.log(`Dictionary: ${ARGS.dictionary}`);
+console.log(`Solver: ${ARGS.solver}`);
+
+const CORS: ResponseInit = {
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, OPTIONS, HEAD",
+  },
+};
 
 Bun.serve({
-  fetch(req: Request): Response | Promise<Response> {
-    return new Response("Hello World!");
+  async fetch(req: Request): Promise<Response> {
+    if (req.method.toUpperCase() != "POST") {
+      return new Response(
+        "Server is up! To solve the board, make POST request with 'board', 'swaps' and 'threads' query params",
+        CORS
+      );
+    }
+    let query = new URL(req.url).searchParams;
+    let board = query.get("board");
+    let swaps = query.get("swaps");
+    let threads = query.get("threads");
+    if (board && swaps && threads) {
+      let text = await Bun.$`${ARGS.solver} -d ${ARGS.dictionary} -t ${threads} -f json -b ${board} -s ${swaps} 2>&1`
+        .nothrow()
+        .text("utf-8");
+      try {
+        let json = JSON.parse(text);
+        return Response.json({ ok: true, data: json }, CORS);
+      } catch (_) {
+        return Response.json({ ok: false, error: text }, CORS);
+      }
+    }
+    return Response.json({ ok: false, error: "Some of required params (board, swaps, threads) are missing" }, CORS);
   },
-  port: args.port,
+  port: ARGS.port,
 });
