5dcfaf5ca1d2208be0198e3e138852b86e9b78ae
Woidly <woidly@proton.me>
2024-12-18 23:01:07 +0900

    refactor(client): move gameplay logic to own class

--- automatic/client/lib/gameplay.ts
+++ automatic/client/lib/gameplay.ts
@@ -1,5 +1,44 @@
 import { solve, stringifyRawBoard } from "./solver";
-import type { Game } from "./types/extern";
+import { type Game, GameState } from "./types/extern";
+
+const GAMEPLAY = new (class GlobalGameplay {
+  game: Game;
+
+  constructor() {
+    // This is safe, since all usages of this.game happen after handleHook sets this.game to actual Game.
+    this.game = {} as Game;
+  }
+
+  play(isMyTurn: boolean) {
+    if (isMyTurn) {
+      setTimeout(() => {
+        let [promise, _] = solve(
+          stringifyRawBoard(this.game.board.boardData),
+          Math.floor(this.game.spellbook.manaCounter.manaCount / 3),
+          12
+        );
+        promise.then((x) => console.log("Solved", x)).catch((e) => console.error("Solver error", e));
+      }, 500);
+    }
+  }
+
+  handleHook(game: Game, isMyTurn: boolean) {
+    this.game = game;
+    //@ts-ignore
+    unsafeWindow._game = this.game;
+    //@ts-ignore
+    unsafeWindow._gg = this;
+    console.log("isMyTurn updated:", isMyTurn);
+    switch (game.currentGameState) {
+      case GameState.MENU:
+        return console.log("In menus");
+      case GameState.GAME:
+        return this.play(isMyTurn);
+      case GameState.GAMEOVER:
+        return console.log("GG!");
+    }
+  }
+})();
 
 function hookCallback(this: Game, isMyTurn: boolean) {
   Object.defineProperty(this, "isMyTurn", {
@@ -10,19 +49,7 @@ function hookCallback(this: Game, isMyTurn: boolean) {
     set: hookCallback,
   });
   if (typeof this.spellbook !== "undefined") {
-    //@ts-ignore
-    unsafeWindow._game = this;
-    console.log("isMyTurn updated:", isMyTurn);
-    if (isMyTurn) {
-      setTimeout(() => {
-        let [promise, _] = solve(
-          stringifyRawBoard(this.board.boardData),
-          Math.floor(this.spellbook.manaCounter.manaCount / 3),
-          12
-        );
-        promise.then((x) => console.log("Solved", x)).catch((e) => console.error("Solver error", e));
-      }, 500);
-    }
+    GAMEPLAY.handleHook(this, isMyTurn);
   }
 }
 
